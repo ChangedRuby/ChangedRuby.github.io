10e9d1e004a1177aa8ec78f21b6b86c4a42991a7
ChangedRuby <gugasboy7@gmail.com>
2025-02-27 14:52:19 -0300

    Added a sidebar that can be open and closed on the left of the screen on smaller devices.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello website!");
+
+const sidebar = document.getElementById('sidebar');
+const sidebar_icon = document.getElementById('sidebar-icon');
+
+function sidebarOpen(){
+    sidebar.style.display = 'block';
+    sidebar_icon.style.display = 'none';
+}
+
+function sidebarClose(){
+    sidebar.style.display = 'none';
+    sidebar_icon.style.display = 'block';
+}

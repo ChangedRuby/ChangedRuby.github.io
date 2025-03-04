f9a38c873d5deef4711fe2df1ef38fd369fb4cb5
ChangedRuby <gugasboy7@gmail.com>
2025-03-04 16:19:11 -0300

    Added a three.js canvas on top of projects dis which displays an animated 3d text that the user can rotate. Added Vite build tool and a new font in a new fonts directory.

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import './styles/style.css'
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const container = document.querySelector('#canvas-container');
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
   alpha: true,
@@ -17,15 +18,43 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(container.clientWidth, container.clientHeight);
 container.appendChild(renderer.domElement);
-camera.position.setY(5);
-camera.position.setZ(30);
+camera.position.setY(0);
+camera.position.setZ(15);
+onWindowResize();
 
 const torus = new THREE.Mesh(
     new THREE.TorusGeometry(10, 3, 16, 100),
     new THREE.MeshStandardMaterial({ color: 0xffff00 }),
 );
 torus.position.set(0, 5, 0);
-scene.add(torus);
+
+var textMesh;
+const fontLoader = new FontLoader();
+fontLoader.load(
+    './fonts/droid_serif_regular.typeface.json',
+    (droidFont) => {
+        const textGeometry = new TextGeometry('Some of my projects', {
+            height: 1,
+            depth: 0.05,
+            size: 1,
+            font: droidFont,
+        });
+        const textMaterial = new THREE.MeshStandardMaterial();
+        textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+        // center the text geometry
+        textGeometry.computeBoundingBox();
+        const center = textGeometry.boundingBox.getCenter(new THREE.Vector3());
+
+        textMesh.updateMatrixWorld();
+        center.applyMatrix4(textMesh.matrixWorld);
+        textGeometry.translate(-center.x, -center.y, -center.z);
+
+        textMesh.position.x = 0;
+        textMesh.position.y = 0;
+        scene.add(textMesh);
+    }
+);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 const pointLight = new THREE.PointLight(0xffffff);
@@ -34,19 +63,25 @@ pointLight.intensity = 10;
 pointLight.position.set(0, 0, 0);
 scene.add(ambientLight, pointLight);
 
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.1;
+controls.enablePan = false;
+
+const startDate = Date.now();
 
 function animate(){
     requestAnimationFrame(animate);
 
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+    var currentDate = Date.now();
+    var milliseconds = currentDate - startDate;
+
+    if(textMesh != undefined){
+        textMesh.rotation.y = Math.sin(milliseconds / 1000) * 0.4;
+    }
     
     controls.update();
 
@@ -55,10 +90,10 @@ function animate(){
 
 // resize renderer to maintain square shape
 function onWindowResize(){
-    const size = Math.min(container.clientWidth, container.clientHeight);
-    camera.aspect = 1; // keeps square aspect ratio
+    // const size = Math.min(container.clientWidth, container.clientHeight);
+    camera.aspect = container.clientWidth / container.clientHeight; // keeps square aspect ratio if 1
     camera.updateProjectionMatrix();
-    renderer.setSize(size, size);
+    renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
 window.addEventListener('resize', onWindowResize);
@@ -67,7 +102,6 @@ if ( WebGL.isWebGL2Available() ) {
 
 	// Initiate function or other initializations here
 	animate();
-    onWindowResize();
 
 } else {
 
